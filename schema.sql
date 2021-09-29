fab68f57bffdf6f5e196fbf747c3017ec8f4f4ec
RobC30 <robcloa30@gmail.com>
2021-09-28 22:03:42 -0400

    added data folder + schema .sql

---
+++ schema.sql
@@ -0,0 +1,54 @@
+-- Creating tables for PH-EmployeeDB
+create table departments (
+	dept_no varchar(4) not null,
+	dept_name varchar(40) not null,
+	primary key (dept_no),
+	unique (dept_name)
+);
+
+create table employees (
+	emp_no int not null,
+	birth_date date not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	gender varchar(1) not null,
+	hire_date date not null,
+	primary key (emp_no)
+);
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
+);
